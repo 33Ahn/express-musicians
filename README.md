a8e6fb656f142aa2eb7520e8b72d869ae722d2ba
Ahn Shields <ahnshields@gmail.com>
2022-09-02 11:16:27 -0700

    Create express routes for CRUD, add Middleware, and update README.md

--- README.md
+++ README.md
@@ -41,6 +41,29 @@ You will be provided with a Musicians DB, along with your database being connect
 
 ## Checklist
 1. Create an express Get Request to an endpoint for "musicians"
-2. Include a Route Parameter id as part of your endpoint;s path/route
+2. Include a Route Parameter id as part of your endpoint's path/route
 3. Reference the Musicians Database and get a musician based on the Route Parameters value
 4. Respond as JSON the referenced musician object
+
+# CRUD and Middleware
+## Assignment
+You’ll be making a request to a Musicians Database using different HTTP Verbs to:
+
+1. Add a musician to the Musicians DB
+
+2. Update a musician on the Musicians DB
+
+3. Delete a musician on the Musicians DB
+
+## Goal
+In the server.js file, begin working after your solution from Get Musicians 2
+
+1. Create express routes that can post a new musician, update a specific musician, and delete a specific musician from the Musicians DB. -- Remember to use async and await
+2. Test your endpoint using Postman and make sure to use the correct HTTP Verb when making your requests to your server.
+3. Recall the Sequelize methods that match up with the Express methods for your particular CRUD operation.
+## Checklist
+1. Include necessary middleware for parsing data provided in the request of the body
+2. Create an express route for creating (adding) a new restaurant on your musician database
+3. Create an express route for updating (replacing) an existing musician with a new musician on your musician database based on ID.
+4. Create an express route for deleting (removing) a musician on your database
+5. Test using Postman that you can Create, Read, Update, and Delete data from your database.
--- server.js
+++ server.js
@@ -6,16 +6,53 @@ const seedMusician = require("./seedData")
 
 const port = 3000;
 
-//TODO
+// Include necessary middleware for parsing data provided in the request of the body
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// TODO
+app.get("/musicians", async (request, response) => {
+    const allMusicians = await Musician.findAll();
+    response.json(allMusicians);
+});
+
 app.get("/musicians/:id", async (request, response) => {
-
     const musician = await Musician.findByPk(request.params.id);
-    response.json(musician);
+    response.json(musician);    
+});
 
-    // with the route < "/musicians"  >
-        // const musicians = await Musician.findAll();  
-        // response.json(musicians);
+// Create an express route for creating (adding) a new musician on your musician database
+
+app.post("/musicians", async (request, response) => {   
+    const newMusician = await Musician.create(request.body);
+    response.json(newMusician);    
+});
+
+// Create an express route for updating (replacing) an existing musician with a new musician on your musician database based on ID.
+
+app.put("/musicians/:id", async (request, response) => {
+    await Musician.update(request.body, {
+        where: {id: request.params.id}});
+    const updatedMusician = await Musician.findByPk(request.params.id);
+    response.json(updatedMusician);
+});
+
+
+// Create an express route for deleting (removing) a musician on your database
+
+app.delete("/musicians/:id", async (request, response) => {
+    const musicianId = await Musician.findByPk(request.params.id);
+    const deletedMusician = await Musician.destroy(musicianId);
+
+    // It worked this way too.
+        // const deletedMusician = await Musician.destroy({where: {
+        //     id: request.params.id
+        // }});
+    response.json(deletedMusician);
 })
+
+
+// Bind the port to my server
 
 app.listen(port, () => {
     sequelize.sync();
