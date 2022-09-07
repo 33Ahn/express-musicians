40de2fd15e00b9fd206236a87f89842363177fd8
Ahn Shields <ahnshields@gmail.com>
2022-09-07 12:30:02 -0700

    Add express router in the main server

---
+++ routes/musicians.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const { Musician } = require("../Musician");
+const router = express.Router();  // initialize the express router
+
+// Add middleware
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+
+// include check, validationResult methods from the Express Validator package in your Express Router for musicians
+const {check, validationResult} = require("express-validator");
+
+
+// Define Routes
+router.get("/", async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+});
+
+router.get("/:id", async (req, res) => {
+    const musicianId = await Musician.findByPk(req.params.id);
+    res.json(musicianId);
+});
+
+router.post("/", [check("name").not().isEmpty().trim(), check("instrument").not().isEmpty().trim(), check("name").isLength({min: 2, max: 20})], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.json({error: errors.array()});
+    }  
+    const newMusician = await Musician.create(req.body);
+    res.json(newMusician);
+    // res.json(await Musician.findAll());    
+});
+
+router.put("/:id", async (req, res) => {  
+    const updatedMusician = await Musician.update(req.body, {where: {id: req.params.id}});      
+    res.json(await Musician.findAll());
+    // res.json(updatedMusician);   // the output is 1
+   
+});
+
+router.delete("/:id", async (req, res) => { 
+    const musicianId = await Musician.findByPk(req.params.id);
+    const deletedMusician = await musicianId.destroy();
+    res.json(deletedMusician);
+
+     // Or write this way        
+    // const deletedMusician = await Musician.destroy({
+    //     where: {
+    //         id: req.params.id   
+    //     }
+    // })  
+    // res.json(await Musician.findAll());   
+    // res.json(deletedMusician); // the outcome is 1
+    
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,57 +3,16 @@ const app = express();
 const {Musician} = require("./Musician")
 const {sequelize} = require("./db")
 const seedMusician = require("./seedData")
-
 const port = 3000;
 
-// Include necessary middleware for parsing data provided in the request of the body
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// include a reference to your router in your main express server
+const musiciansRouter = require("./routes/musicians");
 
-// TODO
-app.get("/musicians", async (request, response) => {
-    const allMusicians = await Musician.findAll();
-    response.json(allMusicians);
-});
-
-app.get("/musicians/:id", async (request, response) => {
-    const musician = await Musician.findByPk(request.params.id);
-    response.json(musician);    
-});
-
-// Create an express route for creating (adding) a new musician on your musician database
-
-app.post("/musicians", async (request, response) => {   
-    const newMusician = await Musician.create(request.body);
-    response.json(newMusician);    
-});
-
-// Create an express route for updating (replacing) an existing musician with a new musician on your musician database based on ID.
-
-app.put("/musicians/:id", async (request, response) => {
-    await Musician.update(request.body, {
-        where: {id: request.params.id}});
-    const updatedMusician = await Musician.findByPk(request.params.id);
-    response.json(updatedMusician);
-});
-
-
-// Create an express route for deleting (removing) a musician on your database
-
-app.delete("/musicians/:id", async (request, response) => {
-    const musicianId = await Musician.findByPk(request.params.id);
-    const deletedMusician = await Musician.destroy(musicianId);
-
-    // It worked this way too.
-        // const deletedMusician = await Musician.destroy({where: {
-        //     id: request.params.id
-        // }});
-    response.json(deletedMusician);
-})
+// Use express router in your main server
+app.use("/musicians", musiciansRouter);
 
 
 // Bind the port to my server
-
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
