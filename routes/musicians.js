6779172c36687b8154d149acb88e6b3e1affba82
Ahn Shields <ahnshields@gmail.com>
2022-09-07 15:41:30 -0700

    Correct indentation

--- routes/musicians.js
+++ routes/musicians.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const { Musician } = require("../Musician");
+const { Musician } = require("../Musician"); 
 const router = express.Router();  // initialize the express router
 
-// Add middleware
+// Add middleware to parse body request
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
-// include check, validationResult methods from the Express Validator package in your Express Router for musicians
-const {check, validationResult} = require("express-validator");
+// include check, validationResult methods from the Express Validator package in your Express Router 
+const { check, validationResult } = require("express-validator");
 
 
 // Define Routes
@@ -21,21 +21,35 @@ router.get("/:id", async (req, res) => {
     res.json(musicianId);
 });
 
-router.post("/", [check("name").not().isEmpty().trim(), check("instrument").not().isEmpty().trim(), check("name").isLength({min: 2, max: 20})], async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        res.json({error: errors.array()});
-    }  
-    const newMusician = await Musician.create(req.body);
-    res.json(newMusician);
-    // res.json(await Musician.findAll());    
+router.post("/", 
+
+    [check("name")
+        .not()
+        .isEmpty()
+        .trim(), 
+
+        check("instrument")
+            .not()
+            .isEmpty()
+            .trim(), 
+
+            check("name")
+                .isLength({min: 2, max: 20})], 
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            res.json({error: errors.array()});
+        }  
+        const newMusician = await Musician.create(req.body);
+        res.json(newMusician);
+        // res.json(await Musician.findAll());    
 });
 
 router.put("/:id", async (req, res) => {  
     const updatedMusician = await Musician.update(req.body, {where: {id: req.params.id}});      
     res.json(await Musician.findAll());
-    // res.json(updatedMusician);   // the output is 1
-   
+    // res.json(updatedMusician);   // the output is 1   
 });
 
 router.delete("/:id", async (req, res) => { 
